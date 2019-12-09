43a05d9351be54118fb386d6a1aa06e8c003446a
ngra <m3.latifi@gmail.com>
2019-12-09 11:16:03 +0330

    End Circle Menu

--- app/src/main/java/com/example/wms/views/fragments/home/FragmentHome.java
+++ app/src/main/java/com/example/wms/views/fragments/home/FragmentHome.java
@@ -5,17 +5,20 @@ Create By Mehrdad Latifi in
 package com.example.wms.views.fragments.home;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.util.Log;
+
+import android.os.Handler;
+import android.view.Display;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -23,36 +26,33 @@ import com.example.wms.R;
 import com.example.wms.databinding.FragmentHomeBinding;
 import com.example.wms.viewmodels.main.FragmentHomeViewModel;
 import com.example.wms.views.activitys.MainActivity;
-import com.example.wms.views.custom.circlemenu.CircleMenuLayout;
-import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.wms.views.custom.textview.VerticalTextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class FragmentHome extends Fragment implements OnChartValueSelectedListener {
+public class FragmentHome extends Fragment {
 
     private Context context;
     private FragmentHomeViewModel fragmentHomeViewModel;
-    private CircleMenuLayout mCircleMenuLayout;
-    private String[] mItemTexts = new String[]{"درخواست جمع آوری", "قرعه کشی", "درخواست پکیج","تفکیک پسماند"};
 
-    @BindView(R.id.fhTextAve)
-    TextView fhTextAve;
+    @BindView(R.id.footerprimery)
+    RelativeLayout footerprimery;
 
-    @BindView(R.id.piechart_1)
-    PieChart pieChart;
+    @BindView(R.id.footer)
+    RelativeLayout footer;
 
+    @BindView(R.id.footerup)
+    TextView footerup;
+
+    @BindView(R.id.footerdown)
+    TextView footerdown;
+
+    @BindView(R.id.footerright)
+    VerticalTextView footerright;
+
+    @BindView(R.id.footerleft)
+    VerticalTextView footerleft;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -75,193 +75,93 @@ public class FragmentHome extends Fragment implements OnChartValueSelectedListen
     @Override
     public void onStart() {//_______________________________________________________________________ Start onStart
         super.onStart();
-
-        pieChart.setUsePercentValues(true);
-        pieChart.getDescription().setEnabled(false);
-        pieChart.setExtraOffsets(0, 0, 0, 0);
-        pieChart.setDragDecelerationFrictionCoef(0.5f);
-        pieChart.setTransparentCircleRadius(50f);
-        pieChart.setHoleColor(context.getResources().getColor(R.color.mlWhite));
-        pieChart.setCenterText(context.getResources().getText(R.string.Letsnotthrowawaythefuture));
-        pieChart.setCenterTextColor(context.getResources().getColor(R.color.colorAccent));
-        pieChart.setCenterTextSizePixels(20f);
-        pieChart.setEntryLabelColor(getResources().getColor(R.color.mlWhite));
-        pieChart.setHoleRadius(30f);
-        pieChart.setTransparentCircleAlpha(30);
-        pieChart.setTransparentCircleRadius(40f);
-        pieChart.setEntryLabelTextSize(12);
-
-        pieChart.setUsePercentValues(false);
-        pieChart.setRotationAngle(45);
-        pieChart.setTouchEnabled(true);
-        pieChart.setClickable(true);
-        pieChart.getLegend().setEnabled(false);
-        pieChart.setOnChartValueSelectedListener(this);
-
-        pieChart.animateY(700, Easing.EaseInCubic);
-        ArrayList<PieEntry> yValues = new ArrayList<>();
-        yValues.add(new PieEntry(30f, mItemTexts[0]));
-        yValues.add(new PieEntry(30f, mItemTexts[1]));
-        yValues.add(new PieEntry(30f, mItemTexts[2]));
-        yValues.add(new PieEntry(30f, mItemTexts[3]));
-
-        List<Integer> colors = new ArrayList<>();
-        colors.add(context.getResources().getColor(R.color.mlHomeMaxScore));
-        colors.add(context.getResources().getColor(R.color.mlHomeAveScore));
-
-        PieDataSet dataSet = new PieDataSet(yValues, "");
-        dataSet.setSliceSpace(5f);
-        dataSet.setSelectionShift(5f);
-        dataSet.setColors(colors);
-
-        PieData pieData = new PieData((dataSet));
-        pieData.setValueTextSize(0f);
-        pieData.setValueTextColor(Color.YELLOW);
-        pieChart.setData(pieData);
-
-
-//        Button btn = (Button) getView().findViewById(R.id.btn);
-//        RadialMenuView radial_menu_view = (RadialMenuView) getView().findViewById(R.id.radial_menu_view);
-//        MenuItemView itemOne = new MenuItemView(context ,"Ask Questions",R.drawable.a, R.color.mlCollectBooth);
-//        MenuItemView itemTwo = new MenuItemView(context,"Friends",R.drawable.b, R.color.mlEdit);
-//        MenuItemView itemThree = new MenuItemView(context,"Gallery", R.drawable.c, R.color.mlHomeAveScore);
-//        MenuItemView itemFour = new MenuItemView(context,"Settings", R.drawable.d, R.color.mlHomeMaxScore);
-//        MenuItemView itemFive = new MenuItemView(context, "Profile", R.drawable.e, R.color.mlCollectRight1);
-//        ArrayList<MenuItemView> items = new ArrayList<>();
-//        items.add(itemOne);
-//        items.add(itemTwo);
-//        items.add(itemThree);
-//        items.add(itemFour);
-//        items.add(itemFive);
-//        radial_menu_view
-//                .setListener(this)
-//                .setMenuItems(items)
-//                .setCenterView(btn)
-//                .setInnerCircle(true, R.color.mlWhite)
-//                .setOffset(10)
-//                .build();
-//
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                radial_menu_view.show();
-//            }
-//        });
-
-//        CustomView ml = (CustomView) getView().findViewById(R.id.ml);
-//
-//        mCircleMenuLayout = (CircleMenuLayout) getView().findViewById(R.id.id_menulayout);
-//        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
-//        mCircleMenuLayout.setOnMenuItemClickListener(new CircleMenuLayout.OnMenuItemClickListener() {
-//
-//            @Override
-//            public void itemClick(View view, int pos) {
-//                switch (pos) {
-//                    case 0:
-//                        MainActivity.FragmentMessage.onNext("Learn");
-//                        break;
-//                    case 1:
-//                        MainActivity.FragmentMessage.onNext("CollectRequest");
-//                        break;
-//
-//                    case 2:
-//                        MainActivity.FragmentMessage.onNext("Lottery");
-//                        break;
-//
-//                    case 3:
-//                        //context.ShowFragmentPAckRequest();
-//                        MainActivity.FragmentMessage.onNext("PckRequest");
-//                        break;
-//                }
-//            }
-//
-//            @Override
-//            public void itemCenterClick(View view) {
-//
-//            }
-//        });
-
-        //ButterKnife.bind(this,getView());
-
+        SetLayout();
+        SetClick();
 
     }//_____________________________________________________________________________________________ End onStart
 
-//    @Override
-//    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
-//        Toast.makeText(context,"onChartGestureStart",Toast.LENGTH_SHORT).show();
-//        Log.i("meri" , "" + me.getButtonState());
-//        Log.i("meri" , "" + me.getFlags());
-//        Log.i("meri" , "" + me.getMetaState());
-//        Log.i("meri" , "" + me.getPointerCount());
-//        Log.i("meri" , "" + me.getPressure());
-//
-//
-//
-//
-//    }
-//
-//    @Override
-//    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
-//    }
-//
-//    @Override
-//    public void onChartLongPressed(MotionEvent me) {
-//        Toast.makeText(context,"onChartLongPressed " ,Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onChartDoubleTapped(MotionEvent me) {
-//        Toast.makeText(context,"onChartDoubleTapped",Toast.LENGTH_SHORT).show();
-//    }
-//
-//    @Override
-//    public void onChartSingleTapped(MotionEvent me) {
-//
-//    }
-//
-//    @Override
-//    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
-//
-//    }
-//
-//    @Override
-//    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
-//
-//    }
-//
-//    @Override
-//    public void onChartTranslate(MotionEvent me, float dX, float dY) {
-//
-//    }
 
 
-    @Override
-    public void onValueSelected(Entry e, Highlight h) {
-        switch ((int) h.getX()) {
-            case 0:
-                MainActivity.FragmentMessage.onNext("CollectRequest");
+    private void SetClick() {//_____________________________________________________________________ Start SetClick
 
-                break;
-            case 1:
-                MainActivity.FragmentMessage.onNext("Lottery");
-                break;
 
-            case 2:
+        footerup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 MainActivity.FragmentMessage.onNext("PckRequest");
-                break;
+            }
+        });
 
-            case 3:
+        footerdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.FragmentMessage.onNext("CollectRequest");
+            }
+        });
+
+        footerleft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.FragmentMessage.onNext("Lottery");
+            }
+        });
+
+        footerright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 MainActivity.FragmentMessage.onNext("Learn");
-                break;
-        }
+            }
+        });
 
 
-    }
 
-    @Override
-    public void onNothingSelected() {
+    }//_____________________________________________________________________________________________ End SetClick
 
-    }
+
+
+    private void SetLayout() {//____________________________________________________________________ Start SetLayout
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int width = footerprimery.getMeasuredWidth();
+                int height = footerprimery.getMeasuredHeight();
+
+                if(width < height)
+                    height = width;
+                else
+                    width = height;
+
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width,height);
+                footer.setLayoutParams(params);
+
+                width = width / 4;
+                ViewGroup.LayoutParams paramsUp = (ViewGroup.LayoutParams) footerup.getLayoutParams();
+                paramsUp.height = width;
+                paramsUp.width = width * 2;
+                footerup.setLayoutParams(paramsUp);
+
+                ViewGroup.LayoutParams paramsDown = (ViewGroup.LayoutParams) footerdown.getLayoutParams();
+                paramsDown.height = width;
+                paramsDown.width = width * 2;
+                footerdown.setLayoutParams(paramsDown);
+
+                ViewGroup.LayoutParams paramsLeft = (ViewGroup.LayoutParams) footerleft.getLayoutParams();
+                paramsLeft.height = width * 2;
+                paramsLeft.width = width;
+                footerleft.setLayoutParams(paramsLeft);
+
+
+                ViewGroup.LayoutParams paramsRight = (ViewGroup.LayoutParams) footerright.getLayoutParams();
+                paramsRight.height = width * 2;
+                paramsRight.width = width;
+                footerright.setLayoutParams(paramsRight);
+
+
+            }
+        },5);
+    }//_____________________________________________________________________________________________ End SetLayout
 
 
 }
