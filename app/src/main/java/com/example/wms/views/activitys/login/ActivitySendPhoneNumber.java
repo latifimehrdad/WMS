04633ceb514ee6cfc6a51d9e168e52aa96b0c4d4
ngra <m3.latifi@gmail.com>
2019-12-10 17:48:09 +0330

    End Curve

--- app/src/main/java/com/example/wms/views/activitys/login/ActivitySendPhoneNumber.java
+++ app/src/main/java/com/example/wms/views/activitys/login/ActivitySendPhoneNumber.java
@@ -1,6 +1,9 @@
 package com.example.wms.views.activitys.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,15 @@ import com.example.wms.R;
 import com.example.wms.databinding.ActivitySendPhoneNumberBinding;
 import com.example.wms.viewmodels.login.ActivitySendPhoneNumberViewModel;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ActivitySendPhoneNumber extends AppCompatActivity {
 
     private ActivitySendPhoneNumberViewModel activitySendPhoneNumberViewModel;
+
+    @BindView(R.id.btnGetVerifyCode)
+    Button btnGetVerifyCode;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,6 +32,21 @@ public class ActivitySendPhoneNumber extends AppCompatActivity {
         activitySendPhoneNumberViewModel = new ActivitySendPhoneNumberViewModel(this);
         binding.setPhonenumber(activitySendPhoneNumberViewModel);
         ButterKnife.bind(this);
+        SetClick();
     }//_____________________________________________________________________________________________ End onCreateView
+
+
+
+    private void SetClick() {//_____________________________________________________________________ Start SetClick
+
+        btnGetVerifyCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivitySendPhoneNumber.this, ActivityVerifyCode.class);
+                startActivity(intent);
+            }
+        });
+
+    }//_____________________________________________________________________________________________ End SetClick
 
 }
