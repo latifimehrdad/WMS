ff326f9fd46f7654f9028dd054241a225807baa3
980902 <m3.latifi@gmail.com>
2020-08-25 15:17:30 +0430

    اضافه کردن نقشه برای انتخاب غرفه

---
+++ app/src/main/java/com/ngra/wms/viewmodels/VM_Map.java
@@ -0,0 +1,13 @@
+package com.ngra.wms.viewmodels;
+
+import android.app.Activity;
+
+public class VM_Map extends VM_Primary {
+
+
+    //______________________________________________________________________________________________ VM_Map
+    public VM_Map(Activity context) {
+        setContext(context);
+    }
+    //______________________________________________________________________________________________ VM_Map
+}
